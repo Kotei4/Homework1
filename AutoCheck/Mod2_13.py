358ef6c0b3d784208118e21e1c1db30ed5866849
Kotei4 <kosorotov.k@gmail.com>
2023-09-09 10:13:08 +0200

    Created New Dir and File

---
+++ AutoCheck/Mod2_13.py
@@ -0,0 +1,19 @@
+message = "Hello my little friends!"
+offset = 37
+encoded_message = ""
+
+i = 0
+while i < 100:
+    #if ch.isalpha():
+    #char = chr(i)
+    #print(char)
+    i += 1
+    break
+       # ch_char = char - ord("a")
+   #     ch_char = (ch_char + offset) % 26 
+    #    new_char = (ch_char + ord("a"))
+    #    encoded_message += chr(new_char)
+ #   else:
+   #     encoded_message += ch
+#print(encoded_message)
+    print(char)
