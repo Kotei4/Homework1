d247eefc73103ea43f30f4719c3fa77d96505a50
Kotei4 <kosorotov.k@gmail.com>
2023-09-14 00:12:35 +0200

    file 15.py - Solution by Kirill, Mod_2_15.py Mine solution

--- AutoCheck/Mod_2_15.py
+++ AutoCheck/Mod_2_15.py
@@ -1,43 +1,50 @@
 result = None
+wait_for_number = True
 operand = None
 operator = None
-wait_for_number = True
 
 while True:
-    char = input(">>>")
+    char = input(">>> ")
 
     if char == "=":
-
         print(f"Result: {result}")
         break
 
-    elif wait_for_number:
-        
+    if wait_for_number:
+
         try:
-            operand = int(char)
-            if result == None:
-                result = operand
+            operand = float(char)
+            wait_for_number = False
 
         except ValueError:
-            print(f"Type a number please")
-        wait_for_number = False
-        #result = operand
-        continue
-
-    elif wait_for_number == False:
-
-        if char in ("-", "+", "/", "*"):
-            operator = char
-            wait_for_number = True
-            
-            if operator == "+":
-                result += operand
-            elif operator == "-":
-                result -= operand
-            elif operator == "/":
-                result /= operand
-            elif operator == "*":
-                result *= operand
+            print(f'{char} is not a number. Try again')
             continue
-    #print(f"{operand} {operator}")
-print(result)
+
+        if result == None:
+            result = operand
+
+        else:
+            if operator == '+':
+                result += operand
+            if operator == '-':
+                result -= operand
+            if operator == '/':
+                result /= operand
+            if operator == '*':
+                result *= operand
+            
+        #print(f'Now result = {result}')
+
+    else:
+
+        if char in (f'{operand} is not \'+\' or \'-\' or \'/\' or \'*\'. Try again'):
+            operator = char
+            
+        else:
+            operator = None
+
+        if operator is None:
+            print(f'{char} Must be \'+\', \'-\', \'/\', \'*\'')
+            
+        else:
+            wait_for_number = True
